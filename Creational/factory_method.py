8784d30f58dd53f692b4e12afce4eb90b63f8e68
segadamyan <adamyanssergey@gmail.com>
2023-04-24 23:26:19 +0400

    Creational Pattern: Factory Method

---
+++ Creational/factory_method.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+
+class Creator(ABC):
+
+    @abstractmethod
+    def factory_method(self):
+        """
+        Creator may also provide some default implementation of the factory method.
+        """
+        pass
+
+    def some_operation(self) -> str:
+        """
+        The Creator's primary responsibility
+        is not creating products. Usually, it contains some core business logic
+        that relies on Product objects, returned by the factory method.
+        Subclasses can indirectly change that business logic by overriding the
+        factory method and returning a different type of product from it.
+        """
+
+        # Call the factory method to create a Product object.
+        product: Product = self.factory_method()
+
+        # Now, use the product.
+        result = f"Creator: The same creator's code has just worked with {product.operation()}"
+
+        return result
+
+
+class ConcreteCreator1(Creator):
+    """
+    Note that the signature of the method still uses the abstract product type,
+    even though the concrete product is actually returned from the method. This
+    way the Creator can stay independent of concrete product classes.
+    """
+
+    def factory_method(self) -> Product:
+        return ConcreteProduct1()
+
+
+class ConcreteCreator2(Creator):
+    def factory_method(self) -> Product:
+        return ConcreteProduct2()
+
+
+class Product(ABC):
+
+    @abstractmethod
+    def operation(self) -> str:
+        pass
+
+
+class ConcreteProduct1(Product):
+    def operation(self) -> str:
+        return "{Result of the ConcreteProduct1}"
+
+
+class ConcreteProduct2(Product):
+    def operation(self) -> str:
+        return "{Result of the ConcreteProduct2}"
+
+
+def client_code(creator: Creator) -> None:
+    """
+    As long as the client keeps working with the creator via
+    the base interface, you can pass it any creator's subclass.
+    """
+
+    print(f"Client: I'm not aware of the creator's class, but it still works.\n"
+          f"{creator.some_operation()}", end="")
+
+
+if __name__ == "__main__":
+    print("App: Launched with the ConcreteCreator1.")
+    client_code(ConcreteCreator1())
+    print("\n")
+
+    print("App: Launched with the ConcreteCreator2.")
+    client_code(ConcreteCreator2())
